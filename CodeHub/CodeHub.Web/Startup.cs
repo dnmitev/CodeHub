bc71d0299c66540afbfef898018d18228fbf3025
Dimitar Mitev <dnmitev@gmail.com>
2014-11-07 08:55:32 +0200

    Initial scafoldded projects

---
+++ CodeHub/CodeHub.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CodeHub.Web.Startup))]
+namespace CodeHub.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

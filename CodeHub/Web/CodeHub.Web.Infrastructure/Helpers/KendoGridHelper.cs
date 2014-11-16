08b99abece2601026e04f475248677591ed83c22
Dimitar Mitev <dnmitev@gmail.com>
2014-11-16 20:53:30 +0200

    Kendo Grid Administration for pastes

---
+++ CodeHub/Web/CodeHub.Web.Infrastructure/Helpers/KendoGridHelper.cs
@@ -0,0 +1,48 @@
+﻿namespace CodeHub.Web.Infrastructure.Helpers
+{
+    using System;
+    using System.Linq.Expressions;
+    using System.Web.Mvc;
+
+    using Kendo.Mvc.UI;
+    using Kendo.Mvc.UI.Fluent;
+
+    public static class KendoGridHelper
+    {
+        public static GridBuilder<T> FullFeaturedGrid<T>(this HtmlHelper helper, string controllerName, Expression<Func<T, object>> modelIdExpression, Action<GridColumnFactory<T>> columns = null, Action<DataSourceModelDescriptorFactory<T>> model = null) where T : class
+        {
+            if (columns == null)
+            {
+                columns = cols =>
+                {
+                    cols.AutoGenerate(true);
+                    cols.Command(c => c.Edit());
+                    cols.Command(c => c.Destroy());
+                };
+            }
+
+            return helper.Kendo()
+                .Grid<T>()
+                .Name("grid")
+                .Columns(columns)
+                .ColumnMenu()
+                .Pageable(page => page.Refresh(true))
+                .Sortable()
+                .Groupable()
+                .Filterable()
+                .Editable(edit => edit.Mode(GridEditMode.PopUp))
+                .Resizable(resize => resize.Columns(true))
+                .DataSource(data =>
+                    data
+                        .Ajax()
+                        .Model(m => m.Id(modelIdExpression))
+                        .ServerOperation(true)
+                        .Events(ev => ev.Error("errorHandler"))
+                        .Model(model)
+                        .Read(read => read.Action("Read", controllerName))
+                        .Update(update => update.Action("Update", controllerName))
+                        .Destroy(destroy => destroy.Action("Destroy", controllerName))
+                        );
+        }
+    }
+}
---
+++ CodeHub/Web/CodeHub.Web/Areas/Administration/Controllers/KendoGridAdministrationController.cs
@@ -0,0 +1,73 @@
+﻿namespace CodeHub.Web.Areas.Administration.Controllers
+{
+    using System.Collections;
+    using System.Data.Entity;
+    using System.Web.Mvc;
+    
+    using AutoMapper;
+    
+    using CodeHub.Data.Contracts;
+
+    using Kendo.Mvc.Extensions;
+    using Kendo.Mvc.UI;
+
+    public abstract class KendoGridAdministrationController : AdminBaseController
+    {
+        public KendoGridAdministrationController(ICodeHubData data)
+            : base(data)
+        {
+        }
+
+        protected abstract IEnumerable GetData();
+
+        protected abstract T GetById<T>(object id) where T : class;
+
+        [HttpPost]
+        public ActionResult Read([DataSourceRequest]DataSourceRequest request)
+        {
+            var readData =
+                this.GetData()
+                .ToDataSourceResult(request);
+
+            return this.Json(readData, JsonRequestBehavior.AllowGet);
+        }
+
+        [NonAction]
+        protected virtual T Create<T>(object model) where T : class
+        {
+            if (model != null && ModelState.IsValid)
+            {
+                var dbModel = Mapper.Map<T>(model);
+                this.ChangeEntityStateAndSave(dbModel, EntityState.Added);
+                return dbModel;
+            }
+
+            return null;
+        }
+
+        [NonAction]
+        protected virtual void Update<TModel, TViewModel>(TViewModel model, object id)
+            where TModel : class
+            where TViewModel : class
+        {
+            if (model != null && ModelState.IsValid)
+            {
+                var dbModel = this.GetById<TModel>(id);
+                Mapper.Map<TViewModel, TModel>(model, dbModel);
+                this.ChangeEntityStateAndSave(dbModel, EntityState.Modified);
+            }
+        }
+
+        protected JsonResult GridOperation<T>(T model, [DataSourceRequest]DataSourceRequest request)
+        {
+            return Json(new[] { model }.ToDataSourceResult(request, ModelState),JsonRequestBehavior.AllowGet);
+        }
+
+        private void ChangeEntityStateAndSave(object dbModel, EntityState state)
+        {
+            var entry = this.Data.Context.Entry(dbModel);
+            entry.State = state;
+            this.Data.SaveChanges();
+        }
+    }
+}
